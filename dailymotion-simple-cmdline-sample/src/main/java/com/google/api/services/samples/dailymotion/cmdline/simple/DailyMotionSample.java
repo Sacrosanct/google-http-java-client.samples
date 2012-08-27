99184d1fb0ea4b520e884af001628dbcc0872dd1
Yaniv Inbar <yanivi@google.com>
2012-08-27 08:01:36 -0400

    [http samples] Update to 1.10.3-beta
    https://codereview.appspot.com/6472052/

--- dailymotion-simple-cmdline-sample/src/main/java/com/google/api/services/samples/dailymotion/cmdline/simple/DailyMotionSample.java
+++ dailymotion-simple-cmdline-sample/src/main/java/com/google/api/services/samples/dailymotion/cmdline/simple/DailyMotionSample.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2011 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -21,8 +21,8 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.http.json.JsonHttpParser;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.client.util.Key;
 
@@ -31,7 +31,7 @@ import java.util.List;
 /**
  * Simple example for the <a href="http://www.dailymotion.com/doc/api/graph-api.html">Dailymotion
  * Graph API</a>.
- *
+ * 
  * @author Yaniv Inbar
  */
 public class DailyMotionSample {
@@ -77,9 +77,9 @@ public class DailyMotionSample {
   private static void run() throws Exception {
     HttpRequestFactory requestFactory =
         HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
-          @Override
+            @Override
           public void initialize(HttpRequest request) {
-            request.addParser(new JsonHttpParser(JSON_FACTORY));
+            request.setParser(new JsonObjectParser(JSON_FACTORY));
           }
         });
     DailyMotionUrl url = new DailyMotionUrl("https://api.dailymotion.com/videos/");
